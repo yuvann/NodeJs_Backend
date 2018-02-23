b669743aac972bb2a194eaf394f298636f3079cf
yuvann <yuvan2244@gmail.com>
2018-02-23 16:10:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname,'Frontend')));	
 app.set('port', (process.env.PORT || 5000))
+app.use('/sample/scripts', express.static(__dirname + '/Frontend/Sample_ReactApp/'));
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
