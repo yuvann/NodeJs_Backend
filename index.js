226056474f3bf6ff6d41179983cbea84993d6505
yuvann <yuvan2244@gmail.com>
2018-02-23 15:36:21 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app            =        express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(express.static(__dirname));	
+app.use(express.static(__dirname+'Frontend'));	
 app.set('port', (process.env.PORT || 5000))
 
 app.use(function(req, res, next) {
@@ -14,8 +14,11 @@ app.use(function(req, res, next) {
 });
 
 app.get('/', function (req, res) {
-  res.send('Welcome Guys')
+  res.send('/Sample         -> React Sample App with HTTP Calls<br>	    /ReviewRestaurant -> View Hotels around your area , Review them , Taste it ( React JS )')
 })
+app.get('/Sample',function(req,res){
+  res.sendFile('/Sample/index.html');
+});
 
 app.use('/api/categories',require('./route/categories.js'));
 
